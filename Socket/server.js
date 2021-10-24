edb816ec441ccce388737503861e69267e6cb22c
Gabriela Miranda <gabi.m.domingues@hotmail.com>
2021-10-24 00:33:06 -0300

    funções comentadas

--- Socket/server.js
+++ Socket/server.js
@@ -15,12 +15,12 @@ app.use('/', (req, res) => {
     res.render('index.html');
 })
 
-// app.get('/screenleft',(request, response)=> {
-//     response.sendFile(__dirname=>'/index.html')
-// });
-// app.get('/screenright',(request, response)=> {
-//     response.sendFile(__dirname=>'/index.html')
-// });
+app.get('/screenleft',(request, response)=> {
+    response.sendFile(__dirname=>'/index.html')
+});
+app.get('/screenright',(request, response)=> {
+    response.sendFile(__dirname=>'/index.html')
+});
 // app.get('/controller', (request, response)=> {
 //     response.sendFile(__dirname=>'/nipple.html')
 // })
@@ -34,27 +34,18 @@ io.on('connection', async(socket)  => {
         sockets_conectados.push(socket.id);
          
         const count2 = io.of("/").sockets.size;
-
-        console.log(`Sockets: ${sockets_conectados} ${count2}`);
+        //console.log(`Sockets: ${sockets_conectados} ${count2}`);
 
         socket.emit('start', count2);
-
-        //socket.broadcast.emit('draw');
 
         if(count2>2){
             socket.disconnect();
             sockets_conectados.pop(socket.id);
         }
 
-        //socket.join("room:" + count2);
         socket.on('disconnect', () => {
             console.log(`Socket desconectado: ${socket.id}`);
             sockets_conectados.pop(socket.id);
-            //socket.disconnect();
-        });
-
-        socket.on('desenharScore', function(ball, y) {
-            socket.broadcast.emit('mudarTela', ball, y);
         });
 
         socket.on('update', function(sc1, sc2,x,y,dx,dy,paddleX,a,b,da,db,paddle2X) {
@@ -65,10 +56,6 @@ io.on('connection', async(socket)  => {
             socket.broadcast.emit('destroyBrick', c,r);
         });
 
-        // socket.on('transmitirTela', function(x,y) {
-        //     socket.broadcast.emit('transmitir', x, y);
-        // });
-
     }
     catch{
         console.log(`Erro na connection!`);
@@ -76,9 +63,3 @@ io.on('connection', async(socket)  => {
     
 });
 server.listen(3000);
-/*
-    Primeiro passo: limitar para duas telas e definir quais são os sockets conectados
-    Segundo passo: ver como será definido o tamanho das telas
-    Terceiro passo: transmitir um dos componentes para outra tela
-    Quarto passo: quando a bola bate na parede manda para a outra tela no width 0
-*/
